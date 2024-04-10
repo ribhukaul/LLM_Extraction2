2cce5ca7959e6b0fc3b6e03672ab53b0a7827acf
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-04-10 11:55:25 +0200

    wamderivaticonfig commit

---
+++ extractors/configs/extraction_config/word_representation/wamderivati/derivatives_wr.py
@@ -0,0 +1,23 @@
+cedola = [
+    "cedola", "cedola", "data di osservazione della cedola",
+    "data di pagamento della cedola condizionale", "importo della cedola condizionale",
+]
+
+sottostanti = ["bloomberg ticker", "ticker", "sottostante", "isin"]
+
+main_info = ["valuta del prodotto", "performance peggiore", "modalità di pagamento"]
+
+sottostanti_table = ["bloomberg ticker", "ticker", "sottostante", "isin"]
+
+first_info_bnp = ["codice isin", "isin", "autorità competente", "redazione", "produttore"]
+
+main_info_bnp = ["data di strike", "strike", "prezzo di emissione", "emissione", "(rimborso)"]
+
+main_info_bnp2 = ["valuta", "valuta del prodotto", "importo nozionale", "nozionale"]
+
+sottostante_bnp = ["bloomberg", "codice bloomberg", "sottostante"]
+
+allegato_bnp_premio = ["premio/i", "premio", "barriera/e", "condizionato"]
+
+allegato_bnp_scadenza = ["liquidazione", "anticipata", "facoltativa", "scadenza, rimborso"]
+
