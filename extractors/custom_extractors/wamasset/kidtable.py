f539b662e57da0c98a91ef1b4bcf4b4c7ce92ec6
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-04-26 13:57:40 +0200

    minima picpac eror handled

--- extractors/custom_extractors/wamasset/kidtable.py
+++ extractors/custom_extractors/wamasset/kidtable.py
@@ -77,8 +77,8 @@ class WamAssetKidFeesExtractor(KidExtractor):
     def extract_picpac(self):
         try:
             extract_picpac_schema = self.extraction_config['tag'].get('picpac')
-            n = int(len(self.text[0])/3)
-            first_page = self.text[0][:n]
+            n = int(len(self.text[0].page_content)/3)
+            first_page = self.text[0].page_content[:n]
             extraction = Models.tag(first_page, extract_picpac_schema, self.file_id)
         except Exception as error:
             print("extract picpac error" + repr(error))
