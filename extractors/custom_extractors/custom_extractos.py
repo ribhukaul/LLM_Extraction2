9394187e88ba16a2a878986c8d81e75240cad672
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-04-30 10:49:50 +0200

    minor error fix

--- extractors/custom_extractors/custom_extractos.py
+++ extractors/custom_extractors/custom_extractos.py
@@ -2,7 +2,7 @@ from extractors.custom_extractors.waminsurance.kid_governance import WamInsuranc
 from extractors.custom_extractors.waminsurance.kid_module import WamInsuranceKidModuleExtractor
 from extractors.custom_extractors.waminsurance.gkid_governance import WamInsuranceGKidGovernanceExtractor
 from extractors.custom_extractors.waminsurance.kid_credem import WamInsuranceKidCredemExtractor
-from extractors.custom_extractors.waminsurance.cga import WamInsuranceCGA
+from extractors.custom_extractors.waminsurance.cgasimple import WamCGA
 from extractors.custom_extractors.wamasset.kidtable import WamAssetKidFeesExtractor
 
 from extractors.custom_extractors.wamderivati.complexity import WamDerivatiComplexity
